b7ad8a85989992ffaf73c833572f4990ed6973e4
Anthony O'Dell <aodell91@gmail.com>
2013-12-24 15:33:48 -0600

    added Tokenizer

---
+++ NBACompiler/Compiler/Tokenizer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BBRTokenizer;
+using Antlr4.Runtime;
+
+namespace Compiler
+{
+    public class Tokenizer
+    {
+        //TODO: this is redundant with the Lexer class
+        private Lexer _lexer;
+
+        public Tokenizer(Lexer lexer)
+        {
+            _lexer = lexer;
+        }
+
+        public CommonTokenStream Tokenize(AntlrInputStream inputStream)
+        {
+            _lexer.SetInputStream(inputStream);
+            return new CommonTokenStream(_lexer);
+        }
+    }
+}
